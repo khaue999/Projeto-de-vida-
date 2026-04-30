dc74c6ac85c16348616e8873a5c7821720a59202
khaue999 <s.khaue@escola.pr.gov.br>
2026-04-30 17:41:08 -0300

    .

--- main.js
+++ main.js
@@ -22,10 +22,14 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-
+function atualizaCronometro(){
 for (let i=0; i<contadores.length;i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);    
+    contadores[i].textContent = calculaTempo(tempos[i]);   
 }
+}
+
+atualizaCronometro();
+setInterval(atualizaCronometro,1000);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
