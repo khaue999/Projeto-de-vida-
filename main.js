5a28ef4776f2531a7c416c8a632112188c0457f6
khaue999 <s.khaue@escola.pr.gov.br>
2026-04-30 17:35:26 -0300

    .

--- main.js
+++ main.js
@@ -15,9 +15,17 @@ for (let i=0;i <botoes,length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-5T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-5T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-5T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);    
+}
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
