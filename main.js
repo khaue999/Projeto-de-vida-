3f8846fa53da715b81304c21f6a51fc61ddf514f
khaue999 <s.khaue@escola.pr.gov.br>
2026-04-30 19:27:30 -0300

    .

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i=0;i <botoes,length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-5T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-5T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2026-10-5T00:00:00");
+const tempoObjetivo2 = new Date("2027-12-30T00:00:00");
+const tempoObjetivo3 = new Date("2028-12-5T00:00:00");
+const tempoObjetivo4 = new Date("2029-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
@@ -53,7 +53,6 @@ function atualizaCronometro(){
                     [i])[2];
                 document.getElementById("seg"+i).textContent = calculaTempo(tempos
                     [i])[3];
-                    console.log("seg"+i);
 
             }
         }
